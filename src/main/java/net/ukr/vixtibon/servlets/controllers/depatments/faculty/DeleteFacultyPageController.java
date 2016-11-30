64e459acf7792e0b93749ae30c2783cc4309a397
AlexandrKovalchuk <vixtibon@ukr.net>
2016-11-30 08:51:53 +0200

    2016.11.30-1

--- src/main/java/net/ukr/vixtibon/servlets/controllers/depatments/faculty/DeleteFacultyPageController.java
+++ src/main/java/net/ukr/vixtibon/servlets/controllers/depatments/faculty/DeleteFacultyPageController.java
@@ -1,9 +1,52 @@
 package net.ukr.vixtibon.servlets.controllers.depatments.faculty;
 
+import net.ukr.vixtibon.base_objects.departments.Faculty;
+import net.ukr.vixtibon.base_objects.departments.Institute;
+import net.ukr.vixtibon.dao.departments.DAOFaculty;
+import net.ukr.vixtibon.dao.departments.DAOInstitute;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by alex on 29/11/2016.
  */
 public class DeleteFacultyPageController  extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if(request.getParameterMap().containsKey("step")){
+            if(request.getParameter("step").equals("step1")){
+                DAOFaculty daoi = new DAOFaculty();
+                Faculty faculty = daoi.getEntityById(Integer.parseInt(request.getParameter("facultyID")));
+                request.setAttribute("selected", "yes");
+                request.setAttribute("faculty", faculty);
+                request.getRequestDispatcher("Admin/Faculty/Operations/DeleteFacultyPage.jsp").forward(request, response);
+            }else if(request.getParameter("step").equals("step2")){
+                DAOFaculty daoi = new DAOFaculty();
+                boolean result = false;
+                result = daoi.delete(Integer.parseInt(request.getParameter("facultyID")));
+                if(result){
+                    request.setAttribute("result", "success");
+                    request.setAttribute("menu", "faculty");
+                }else{
+                    request.setAttribute("menu", "faculty");
+                    request.setAttribute("result", "unsuccess");
+                }
+                request.getRequestDispatcher("ActionResultPageController").forward(request, response);
+            }else if(request.getParameter("step").equals("cancel")){
+                request.getRequestDispatcher("FacultyPageController").forward(request, response);
+            }else{
+                //error page
+            }
+        }else{
+            DAOInstitute daoi = new DAOInstitute();
+            ArrayList<Institute> i = daoi.getAllWithFaculties();
+            System.out.println("i length " + i.size());
+            request.setAttribute("institutesList", i);
+            request.getRequestDispatcher("Admin/Faculty/Operations/DeleteFacultyPage.jsp").forward(request, response);
+        }
+    }
 }
---
+++ web/Admin/Faculty/Operations/DeleteFacultyPage.jsp
@@ -0,0 +1,65 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: alex
+  Date: 30/11/2016
+  Time: 08:33
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="Admin/css/admin_styles.css">
+    <title>Delete Faculty Page</title>
+</head>
+<body>
+<div class = "pageTitleText">
+    <h5>
+        Delete Faculty Page!
+        <br />
+        <c:if test = "${selected ne 'yes'}">
+            Select Faculty to Delete:
+            <c:forEach items="${institutesList}" var="institute">
+                <div>
+                    <h5><c:out value="${institute.getLongName()}"/></h5>
+                    <c:forEach items="${institute.getFacultys()}" var="faculty">
+                        <form action="/DeleteFacultyPageController" method="post" accept-charset="UTF-8">
+                            <input type="hidden"  name="step" value="step1">
+                            <input type="hidden"  name="facultyID" value="${faculty.getID()}">
+                            <button onclick="submit" class="itemButton" ><h2><c:out value="${faculty.getLongName()}"/></h2></button>
+                        </form>
+
+                    </c:forEach>
+                </div>
+            </c:forEach>
+        </c:if>
+        <c:if test = "${selected eq 'yes'}">
+            Delete Faculty: <h5><c:out value="${faculty.getLongName()}"/></h5>
+            <div>
+                <form action="/DeleteFacultyPageController" method="post" accept-charset="UTF-8">
+                    <table>
+                        <input type="hidden"  name="step" value="step2">
+                        <input type="hidden"  name="facultyID" value="${faculty.getID()}" >
+                        <tr>
+                            <td colspan=2>
+                                <button onclick="submit"  class="controlButton"><h2>Delete</h2></button>
+                            </td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
+        </c:if>
+    </h5>
+</div>
+<div>
+    <form action="DeleteFacultyPageController" method="post">
+        <input type="hidden"  name="step" value="cancel">
+        <td colspan=2>
+            <button onclick="submit"  class="topicButton"><h2>Cancel</h2></button>
+        </td>
+    </form>
+</div>
+<br />
+
+</body>
+</html>
